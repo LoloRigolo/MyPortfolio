84fb61204199b2ea4d2eb3b7833df7661f2a70a0
Lorenzo <lorenzo.aversano@edu.devinci.fr>
2022-01-01 17:49:01 +0100

    :bug: Try to add more than one slide on the page

--- script/script.js
+++ script/script.js
@@ -12,7 +12,6 @@ ham.addEventListener("click", toggleHamburger); // On crée une fonction lors d'
 
 if(document.title == "Projets"){
 
-    
   var slideIndex = 1;
   showSlides(slideIndex);
   
@@ -27,6 +26,35 @@ if(document.title == "Projets"){
   function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+  }
+
+
+  var slideIndex = 1;
+  showSlide(slideIndex);
+  
+  function plusSlides(n) {
+    showSlide(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlide(slideIndex = n);
+  }
+  
+    
+  function showSlide(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlide");
     var dots = document.getElementsByClassName("dot");
     if (n > slides.length) {slideIndex = 1}    
     if (n < 1) {slideIndex = slides.length}
